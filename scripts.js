305ad00dd52b1fa5bf32e58b054cf972869de794
venkateshkrishnak-vibed <venkatesh.krishna.k@gmail.com>
2025-08-31 02:37:21 +0530

    shrisn_website

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function toggleFAQ(faqId) {
+    const content = document.getElementById(faqId + '-content');
+    const icon = document.getElementById(faqId + '-icon');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    } else {
+        content.classList.add('hidden');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    }
+}
