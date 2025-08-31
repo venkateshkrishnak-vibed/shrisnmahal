ae12420f703493c368b63ac18962f973ad4af662
Venkatesh Krishnakumar <venkatesh.krishna.k@gmail.com>
2025-09-01 03:34:54 +0530

    VIDEO EMBEDDING

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
+// Video carousel logic for 'Experience our venue' section
+document.addEventListener('DOMContentLoaded', function() {
+    const videoSources = [
+        'https://www.youtube.com/embed/EZewGOc-fho?rel=0&modestbranding=1',
+        'https://www.youtube.com/embed/yIP3Wz1JgRo?rel=0&modestbranding=1',
+        'https://www.youtube.com/embed/cx2OITPwfXQ?rel=0&modestbranding=1',
+        'https://www.youtube.com/embed/xwp81L5kXys?rel=0&modestbranding=1'
+    ];
+    
+    let currentVideo = 0;
+    const videoElement = document.getElementById('venue-video');
+    const leftBtn = document.getElementById('video-left');
+    const rightBtn = document.getElementById('video-right');
+    
+    function showVideo(idx) {
+        if (videoElement) {
+            videoElement.src = videoSources[idx];
+        }
+    }
+    
+    if (leftBtn && rightBtn && videoElement) {
+        leftBtn.addEventListener('click', function() {
+            currentVideo = (currentVideo - 1 + videoSources.length) % videoSources.length;
+            showVideo(currentVideo);
+        });
+        rightBtn.addEventListener('click', function() {
+            currentVideo = (currentVideo + 1) % videoSources.length;
+            showVideo(currentVideo);
+        });
+        showVideo(currentVideo);
+    }
+});
 function toggleFAQ(faqId) {
     const content = document.getElementById(faqId + '-content');
     const icon = document.getElementById(faqId + '-icon');
